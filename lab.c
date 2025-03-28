4942a06e93b8ced63977f8901347bfd25e9995b2
premsagarreddy-creator <g.reddy2@s.amity.edu>
2025-03-28 12:22:05 +0530

    updated 3 lines

---
+++ lab.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    int choice
+    printf("Enter a number (1-3): ");
+    scanf("%d", &choice);
+    switch (choice) {
+        case 1:
+            printf("You selected One.\n");
+            break;
+ 
+}
